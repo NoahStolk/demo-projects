6d56652ef65eec67cf3df92b7c2df73b2722c273
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-09 21:42:11 +0200

    Remove unnecessary content

--- blazor/ChromelyBlazor.WebAssembly/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-global using ChromelyBlazor.WebAssembly;
--- blazor/ChromelyBlazor.WebAssemblyApp/Program.cs
+++ blazor/ChromelyBlazor.WebAssemblyApp/Program.cs
@@ -1,16 +1,20 @@
-﻿// Optional config
+﻿using Chromely.Core;
+using Chromely.Core.Configuration;
+using Chromely.Core.Infrastructure;
+using Chromely.Core.Network;
+using Chromely.NativeHosts;
+using ChromelyBlazor.WebAssemblyApp;
+
 var config = DefaultConfiguration.CreateForRuntimePlatform();
 config.WindowOptions.Title = "blazor components demo";
 config.UrlSchemes.Add(new UrlScheme(DefaultSchemeName.LOCALREQUEST, "http", "app", string.Empty, UrlSchemeType.LocalResource, false));
 config.StartUrl = "http://app/index.html";
 
-
 ThreadApt.STA();
 
 AppBuilder
-    .Create(args)
-    .UseConfig<DefaultConfiguration>(config)
-    .UseWindow<WindowSample>()
-    .UseApp<AppSample>()
-    .Build()
-    .Run();
+	.Create(args)
+	.UseConfig<DefaultConfiguration>(config)
+	.UseApp<AppSample>()
+	.Build()
+	.Run();
